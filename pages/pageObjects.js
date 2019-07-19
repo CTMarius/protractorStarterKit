bc2ae50785fd7d21f0420cfb559244de7afeb4bf
Marius <mt.cristea@gmail.com>
2019-07-19 13:32:32 +0200

    Update pageObjects.js

--- pages/pageObjects.js
+++ pages/pageObjects.js
@@ -4,6 +4,7 @@ module.exports = {
 
     elemOne: element(by.id('')),
     elemTwo: element(by.css('')),
-    elemThree: element(by.css('')),
+    elemThree: element(by.cssContainingText('','')),
+    elemFour: element(by.xpath('')),
 
 };
