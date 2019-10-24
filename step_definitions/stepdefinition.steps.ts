6fbe27cef9ee6ee9bb64083ba9dbb70295289c9f
Marius <mt.cristea@gmail.com>
2019-10-24 09:45:35 +0200

    Update stepdefinition.steps.ts

--- step_definitions/stepdefinition.steps.ts
+++ step_definitions/stepdefinition.steps.ts
@@ -5,6 +5,7 @@ import AppPage = require('../pages/pageObjects');
 import { Given, Then, When } from "cucumber";
 var {setDefaultTimeout} = require('cucumber');
 setDefaultTimeout(60 * 1000);
+
 var pageObject: AppPage = new AppPage();
 
     Given(/^I navigate to the login page$/, async() => {
