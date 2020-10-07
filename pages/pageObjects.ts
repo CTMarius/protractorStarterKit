a60fe1d2eddbe76303996585978158fe025d6645
Marius <mt.cristea@gmail.com>
2020-10-07 19:20:28 +0200

    Update pageObjects.ts

--- pages/pageObjects.ts
+++ pages/pageObjects.ts
@@ -3,10 +3,12 @@ import { browser, element, by, ExpectedConditions, ElementFinder, $} from 'protr
 class AppPage {
     public loginButton: ElementFinder;
     public emailField: ElementFinder;
+    public acceptCookies : ElementFinder;
 
     constructor() {
         this.loginButton = element(by.xpath("//div/a[contains(., 'Sign In')]"));
         this.emailField = element(by.id("id_userLoginId"));
+        this.acceptCookies = element(by.classname("btn-red"));
         
     }
 }
