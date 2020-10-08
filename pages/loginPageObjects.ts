01b0ca5968ec0dce4ab90275e19fd9de5fca9b91
Marius <mt.cristea@gmail.com>
2020-10-08 11:33:15 +0200

    Update loginPageObjects.ts

--- pages/loginPageObjects.ts
+++ pages/loginPageObjects.ts
@@ -4,13 +4,13 @@ class LoginPage {
     public loginButton: ElementFinder;
     public emailField: ElementFinder;
     public acceptCookies: ElementFinder;    
-    //TODO: Define bellow the elements for the password field and the submit button. Use the existing elements as an example.  
+    //Define below this line the elements for the password field and the submit button. Use the existing elements as an example.  
         
     constructor() {
         this.loginButton = element(by.xpath("//div/a[contains(., 'Sign In')]"));
         this.emailField = element(by.id("id_userLoginId"));
         this.acceptCookies = element(by.className("btn-red"));
-        //Add constructors for your elements bellow this line. Use the existing constructors as an example.
+        //Add constructors for your elements below this line. Use the existing constructors as an example.
         
     }
 }
