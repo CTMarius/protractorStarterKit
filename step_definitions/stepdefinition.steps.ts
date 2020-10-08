20c7e96b959e8965f3a64fa45ce058388eed6eee
Marius <mt.cristea@gmail.com>
2020-10-08 08:00:47 +0200

    Update stepdefinition.steps.ts

--- step_definitions/stepdefinition.steps.ts
+++ step_definitions/stepdefinition.steps.ts
@@ -5,7 +5,7 @@ import { assert } from 'chai';
 import { Given, Then, When } from "cucumber";
 import LoginPage = require('../pages/loginPageObjects');
 
-var LoginPage: LoginPage = new LoginPage();
+var loginPage: LoginPage = new LoginPage();
 var {setDefaultTimeout} = require('cucumber');
 
 setDefaultTimeout(60 * 1000);
@@ -13,21 +13,21 @@ browser.waitForAngularEnabled(false);
 
     Given(/^I navigate to the main page$/, async() => {        
         await browser.get('https://www.netflix.com').then(async() => {
-            if(LoginPage.acceptCookies.isPresent()){
-                LoginPage.acceptCookies.click();
+            if(loginPage.acceptCookies.isPresent()){
+                loginPage.acceptCookies.click();
             }
-            await LoginPage.loginButton.isPresent().then(async(result) =>{
+            await loginPage.loginButton.isPresent().then(async(result) =>{
                 return expect(result).to.be.true;
             })
         })
     });
 
     When(/^I type "(.*?)" in the email field$/, async(email) => {             
-       await LoginPage.emailField.isPresent().then(async(result) =>{
-          await LoginPage.emailField.sendKeys(email);
+       await loginPage.emailField.isPresent().then(async(result) =>{
+          await loginPage.emailField.sendKeys(email);
         })        
     });
 
     When(/^I click on the sign in button$/, async() => {        
-        return LoginPage.loginButton.click();
+        return loginPage.loginButton.click();
     });
