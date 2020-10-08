cb9e07f8ebb635addf0732762f39f6d5453013e1
Marius <mt.cristea@gmail.com>
2020-10-08 07:57:25 +0200

    Update stepdefinition.steps.ts

--- step_definitions/stepdefinition.steps.ts
+++ step_definitions/stepdefinition.steps.ts
@@ -3,9 +3,9 @@ import { browser } from 'protractor/built';
 import { expect } from 'chai';
 import { assert } from 'chai';
 import { Given, Then, When } from "cucumber";
-import AppPage = require('../pages/pageObjects');
+import LoginPage = require('../pages/loginPageObjects');
 
-var pageObject: AppPage = new AppPage();
+var LoginPage: LoginPage = new LoginPage();
 var {setDefaultTimeout} = require('cucumber');
 
 setDefaultTimeout(60 * 1000);
@@ -13,21 +13,21 @@ browser.waitForAngularEnabled(false);
 
     Given(/^I navigate to the main page$/, async() => {        
         await browser.get('https://www.netflix.com').then(async() => {
-            if(pageObject.acceptCookies.isPresent()){
-                pageObject.acceptCookies.click();
+            if(LoginPage.acceptCookies.isPresent()){
+                LoginPage.acceptCookies.click();
             }
-            await pageObject.loginButton.isPresent().then(async(result) =>{
+            await LoginPage.loginButton.isPresent().then(async(result) =>{
                 return expect(result).to.be.true;
             })
         })
     });
 
     When(/^I type "(.*?)" in the email field$/, async(email) => {             
-       await pageObject.emailField.isPresent().then(async(result) =>{
-          await pageObject.emailField.sendKeys(email);
+       await LoginPage.emailField.isPresent().then(async(result) =>{
+          await LoginPage.emailField.sendKeys(email);
         })        
     });
 
     When(/^I click on the sign in button$/, async() => {        
-        return pageObject.loginButton.click();
+        return LoginPage.loginButton.click();
     });
