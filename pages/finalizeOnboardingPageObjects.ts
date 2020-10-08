5727cd86b061bacda9908e122d352d4264c8a581
Marius <mt.cristea@gmail.com>
2020-10-08 08:05:46 +0200

    Update finalizeOnboardingPageObjects.ts

--- pages/finalizeOnboardingPageObjects.ts
+++ pages/finalizeOnboardingPageObjects.ts
@@ -2,6 +2,9 @@
 import { browser, element, by, ExpectedConditions, ElementFinder, $} from 'protractor';
 
 class FinalizeOnboardingPage {
+    
+    //TODO: define here the elements required to perform actions in the finalize account page. Use LoginPage as example.
+    
     public elementName: ElementFinder;
     
     constructor() {
